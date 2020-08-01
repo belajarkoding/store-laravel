59054dc283da11a344447f8f381ef2c8e57f8167
Galih Pratama <hanamura.iost@gmail.com>
2020-08-01 11:21:53 +0700

    Navbar update

--- resources/views/includes/navbar-auth.blade.php
+++ resources/views/includes/navbar-auth.blade.php
@@ -3,7 +3,7 @@
       data-aos="fade-down"
     >
       <div class="container">
-        <a href="{{  url('home') }}" class="navbar-brand">
+        <a href="{{ route('home') }}" class="navbar-brand">
           <img src="/images/logo.svg" alt="Logo" />
         </a>
         <button
@@ -17,10 +17,10 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
-              <a href="{{  url('home') }}" class="nav-link">Home</a>
+              <a href="{{  route('home') }}" class="nav-link">Home</a>
             </li>
             <li class="nav-item">
-              <a href="{{  url('categories') }}" class="nav-link">Categories</a>
+              <a href="{{  route('categories') }}" class="nav-link">Categories</a>
             </li>
             <li class="nav-item">
               <a href="#" class="nav-link">Rewards</a>
--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -25,17 +25,74 @@
             <li class="nav-item">
                 <a href="#" class="nav-link">Rewards</a>
             </li>
-            <li class="nav-item">
-                <a href="/register.html" class="nav-link">Sign Up</a>
-            </li>
-            <li class="nav-item">
-                <a
-                href="/login.html"
-                class="btn btn-success nav-link px-4 text-white"
-                >Sign In</a
-                >
-            </li>
+            @guest
+                <li class="nav-item">
+                    <a href="{{ route('register') }}" class="nav-link">Sign Up</a>
+                </li>
+                <li class="nav-item">
+                    <a
+                    href="{{  route('login') }}"
+                    class="btn btn-success nav-link px-4 text-white"
+                    >Sign In</a
+                    >
+                </li>
+            @endguest
         </ul>
+
+        @auth
+            <!-- Desktop Menu -->
+            <ul class="navbar-nav d-none d-lg-flex">
+                <li class="nav-item dropdown">
+                    <a
+                        href="#"
+                        class="nav-link"
+                        id="navbarDropdown"
+                        role="button"
+                        data-toggle="dropdown"
+                    >
+                        <img
+                            src="/images/icon-user.png"
+                            alt=""
+                            class="rounded-circle mr-2 profile-picture"
+                        />
+                        Hi, {{ Auth::user()->name }}
+                    </a>
+                    <div class="dropdown-menu">
+                        <a href="{{ route('dashboard') }}" class="dropdown-item">Dashboard</a>
+                        <a href="{{ route('dashboard-settings-account') }}" class="dropdown-item">
+                            Settings
+                        </a>
+                        <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                               Logout
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('cart') }}" class="nav-link d-inline-block mt-2">
+                        <img src="/images/icon-cart-empty.svg" alt="" />
+                    </a>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav d-block d-lg-none">
+                <li class="nav-item">
+                    <a href="{{ route('dashboard') }}" class="nav-link">
+                        Hi, {{ Auth::user()->name }}
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('cart') }}" class="nav-link d-inline-block">
+                        Cart
+                    </a>
+                </li>
+            </ul>    
+        @endauth
+        
     </div>
     </div>
 </nav>
