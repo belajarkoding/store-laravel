eea63c2b6031bd89978a2209c116b924f233cc19
Galih Pratama <hanamura.iost@gmail.com>
2020-08-11 15:19:53 +0700

    Update Cart Pada Navigasi

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -74,7 +74,15 @@
                 </li>
                 <li class="nav-item">
                     <a href="{{ route('cart') }}" class="nav-link d-inline-block mt-2">
-                        <img src="/images/icon-cart-empty.svg" alt="" />
+                        @php
+                            $carts = \App\Cart::where('users_id', Auth::user()->id)->count();
+                        @endphp
+                        @if($carts > 0)
+                            <img src="/images/icon-cart-filled.svg" alt="" />
+                            <div class="card-badge">{{ $carts }}</div>
+                        @else
+                            <img src="/images/icon-cart-empty.svg" alt="" />
+                        @endif
                     </a>
                 </li>
             </ul>
