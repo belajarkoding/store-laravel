8bd455f7390db395867034def4dc94956dfd11dd
Galih Pratama <hanamura.iost@gmail.com>
2020-08-11 14:54:17 +0700

    Form Profile User

--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -44,35 +44,37 @@
                   </tr>
                 </thead>
                 <tbody>
+                  @php $totalPrice = 0 @endphp
                   @foreach ($carts as $cart)
-                      <tr>
-                        <td style="width: 20%;">
-                          @if($cart->product->galleries)
-                            <img
-                              src="{{ Storage::url($cart->product->galleries->first()->photos) }}"
-                              alt=""
-                              class="cart-image"
-                            />
-                          @endif
-                        </td>
-                        <td style="width: 35%;">
-                          <div class="product-title">{{ $cart->product->name }}</div>
-                          <div class="product-subtitle">by {{ $cart->product->user->store_name }}</div>
-                        </td>
-                        <td style="width: 35%;">
-                          <div class="product-title">${{ number_format($cart->product->price) }}</div>
-                          <div class="product-subtitle">USD</div>
-                        </td>
-                        <td style="width: 20%;">
-                          <form action="{{ route('cart-delete', $cart->products_id) }}" method="POST">
-                            @method('DELETE')
-                            @csrf
-                            <button class="btn btn-remove-cart" type="submit">
-                              Remove
-                            </button>
-                          </form>
-                        </td>
-                      </tr>
+                    <tr>
+                      <td style="width: 20%;">
+                        @if($cart->product->galleries)
+                          <img
+                            src="{{ Storage::url($cart->product->galleries->first()->photos) }}"
+                            alt=""
+                            class="cart-image"
+                          />
+                        @endif
+                      </td>
+                      <td style="width: 35%;">
+                        <div class="product-title">{{ $cart->product->name }}</div>
+                        <div class="product-subtitle">by {{ $cart->product->user->store_name }}</div>
+                      </td>
+                      <td style="width: 35%;">
+                        <div class="product-title">${{ number_format($cart->product->price) }}</div>
+                        <div class="product-subtitle">USD</div>
+                      </td>
+                      <td style="width: 20%;">
+                        <form action="{{ route('cart-delete', $cart->products_id) }}" method="POST">
+                          @method('DELETE')
+                          @csrf
+                          <button class="btn btn-remove-cart" type="submit">
+                            Remove
+                          </button>
+                        </form>
+                      </td>
+                    </tr>
+                    @php $totalPrice += $cart->product->price @endphp
                   @endforeach
                 </tbody>
               </table>
@@ -86,119 +88,165 @@
               <h2 class="mb-4">Shipping Details</h2>
             </div>
           </div>
-          <div class="row mb-2" data-aos="fade-up" data-aos-delay="200">
-            <div class="col-md-6">
-              <div class="form-group">
-                <label for="addressOne">Address 1</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="addressOne"
-                  name="addressOne"
-                  value="Setra Duta Cemara"
-                />
+          <form action="" method="POST" enctype="multipart/form-data">
+            <div class="row mb-2" data-aos="fade-up" data-aos-delay="200" id="locations">
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="address_one">Address 1</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="address_one"
+                    name="address_one"
+                    value="Setra Duta Cemara"
+                  />
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="address_two">Address 2</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="address_two"
+                    name="address_two"
+                    value="Blok B2 No. 34"
+                  />
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="form-group">
+                  <label for="provinces_id">Province</label>
+                  <select name="provinces_id" id="provinces_id" class="form-control" v-model="provinces_id" v-if="provinces">
+                    <option v-for="province in provinces" :value="province.id">@{{ province.name }}</option>
+                  </select>
+                  <select v-else class="form-control"></select>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="form-group">
+                  <label for="regencies_id">City</label>
+                  <select name="regencies_id" id="regencies_id" class="form-control" v-model="regencies_id" v-if="regencies">
+                    <option v-for="regency in regencies" :value="regency.id">@{{regency.name }}</option>
+                  </select>
+                  <select v-else class="form-control"></select>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="form-group">
+                  <label for="zip_code">Postal Code</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="zip_code"
+                    name="zip_code"
+                    value="40512"
+                  />
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="country">Country</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="country"
+                    name="country"
+                    value="Indonesia"
+                  />
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="phone_number">Mobile</label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="phone_number"
+                    name="phone_number"
+                    value="+628 2020 11111"
+                  />
+                </div>
               </div>
             </div>
-            <div class="col-md-6">
-              <div class="form-group">
-                <label for="addressTwo">Address 2</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="addressTwo"
-                  name="addressTwo"
-                  value="Blok B2 No. 34"
-                />
+            <div class="row" data-aos="fade-up" data-aos-delay="150">
+              <div class="col-12">
+                <hr />
+              </div>
+              <div class="col-12">
+                <h2 class="mb-1">Payment Informations</h2>
               </div>
             </div>
-            <div class="col-md-4">
-              <div class="form-group">
-                <label for="province">Province</label>
-                <select name="province" id="province" class="form-control">
-                  <option value="West Java">West Java</option>
-                </select>
+            <div class="row" data-aos="fade-up" data-aos-delay="200">
+              <div class="col-4 col-md-2">
+                <div class="product-title">$0</div>
+                <div class="product-subtitle">Country Tax</div>
+              </div>
+              <div class="col-4 col-md-3">
+                <div class="product-title">$0</div>
+                <div class="product-subtitle">Product Insurance</div>
+              </div>
+              <div class="col-4 col-md-2">
+                <div class="product-title">$0</div>
+                <div class="product-subtitle">Ship to Jakarta</div>
+              </div>
+              <div class="col-4 col-md-2">
+                <div class="product-title text-success">${{ number_format($totalPrice ?? 0) }}</div>
+                <div class="product-subtitle">Total</div>
+              </div>
+              <div class="col-8 col-md-3">
+                <a
+                  href="/success.html"
+                  class="btn btn-success mt-4 px-4 btn-block"
+                >
+                  Checkout Now
+                </a>
               </div>
             </div>
-            <div class="col-md-4">
-              <div class="form-group">
-                <label for="city">City</label>
-                <select name="city" id="city" class="form-control">
-                  <option value="Bandung">Bandung</option>
-                </select>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="form-group">
-                <label for="postalCode">Postal Code</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="postalCode"
-                  name="postalCode"
-                  value="40512"
-                />
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="form-group">
-                <label for="country">Country</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="country"
-                  name="country"
-                  value="Indonesia"
-                />
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="form-group">
-                <label for="mobile">Mobile</label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="mobile"
-                  name="mobile"
-                  value="+628 2020 11111"
-                />
-              </div>
-            </div>
-          </div>
-          <div class="row" data-aos="fade-up" data-aos-delay="150">
-            <div class="col-12">
-              <hr />
-            </div>
-            <div class="col-12">
-              <h2 class="mb-1">Payment Informations</h2>
-            </div>
-          </div>
-          <div class="row" data-aos="fade-up" data-aos-delay="200">
-            <div class="col-4 col-md-2">
-              <div class="product-title">$10</div>
-              <div class="product-subtitle">Country Tax</div>
-            </div>
-            <div class="col-4 col-md-3">
-              <div class="product-title">$280</div>
-              <div class="product-subtitle">Product Insurance</div>
-            </div>
-            <div class="col-4 col-md-2">
-              <div class="product-title">$580</div>
-              <div class="product-subtitle">Ship to Jakarta</div>
-            </div>
-            <div class="col-4 col-md-2">
-              <div class="product-title text-success">$392,409</div>
-              <div class="product-subtitle">Total</div>
-            </div>
-            <div class="col-8 col-md-3">
-              <a
-                href="/success.html"
-                class="btn btn-success mt-4 px-4 btn-block"
-              >
-                Checkout Now
-              </a>
-            </div>
-          </div>
+          </form>
         </div>
       </section>
     </div>
 @endsection
+
+@push('addon-script')
+    <script src="/vendor/vue/vue.js"></script>
+    <script src="https://unpkg.com/vue-toasted"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <script>
+      var locations = new Vue({
+        el: "#locations",
+        mounted() {
+          this.getProvincesData();
+        },
+        data: {
+          provinces: null,
+          regencies: null,
+          provinces_id: null,
+          regencies_id: null,
+        },
+        methods: {
+          getProvincesData() {
+            var self = this;
+            axios.get('{{ route('api-provinces') }}')
+              .then(function (response) {
+                  self.provinces = response.data;
+              })
+          },
+          getRegenciesData() {
+            var self = this;
+            axios.get('{{ url('api/regencies') }}/' + self.provinces_id)
+              .then(function (response) {
+                  self.regencies = response.data;
+              })
+          },
+        },
+        watch: {
+          provinces_id: function (val, oldVal) {
+            this.regencies_id = null;
+            this.getRegenciesData();
+          },
+        }
+      });
+    </script>
+@endpush
