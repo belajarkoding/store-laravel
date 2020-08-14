e61b0d2bb4af9ff2d165b25a4f90374549ef2447
Galih Pratama <hanamura.iost@gmail.com>
2020-08-14 20:54:40 +0700

    Konfigurasi Library Midtrans

--- config/services.php
+++ config/services.php
@@ -29,5 +29,12 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'midtrans' => [
+        'serverKey'     => env('MIDTRANS_SERVER_KEY'),
+        'clientKey'     => env('MIDTRANS_CLIENT_KEY'),
+        'isProduction'  => env('MIDTRANS_IS_PRODUCTION', false),
+        'isSanitized'   => env('MIDTRANS_IS_SANITIZED', true),
+        'is3ds'         => env('MIDTRANS_IS_3DS', true),                
+    ],
 
 ];
