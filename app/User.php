06959bde5f04f0265b5d6427e1dbefd33e2eaba7
Galih Pratama <hanamura.iost@gmail.com>
2020-08-14 20:55:48 +0700

    Mengubah Model User

--- app/User.php
+++ app/User.php
@@ -16,7 +16,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'roles', 'store_name', 'categories_id', 'store_status'
+        'name', 'email', 'password', 'roles', 'store_name', 'categories_id', 'store_status',
+        'address_one', 'address_two', 'provinces_id', 'regencies_id', 'zip_code', 'country', 'phone_number'
     ];
 
     /**
