fd735095a4030ea2af50d65743515b496ab26ebb
Galih Pratama <hanamura.iost@gmail.com>
2020-08-14 20:55:28 +0700

    Mengubah Model Transaction

--- app/Transaction.php
+++ app/Transaction.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = [
+        'users_id', 
+        'inscurance_price',
+        'shipping_price',
+        'total_price',
+        'transaction_status',
+        'code'
+    ];
+
+    protected $hidden = [
+
+    ];
 }
