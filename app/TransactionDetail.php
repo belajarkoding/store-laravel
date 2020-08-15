6c30f8ebc71449e5adac893c45787964b79f86f3
Galih Pratama <hanamura.iost@gmail.com>
2020-08-15 18:07:06 +0700

    Mengubah Model Transaction Detail

--- app/TransactionDetail.php
+++ app/TransactionDetail.php
@@ -18,4 +18,12 @@ class TransactionDetail extends Model
     protected $hidden = [
 
     ];
+
+    public function product(){
+        return $this->hasOne( Product::class, 'id', 'products_id' );
+    }
+
+    public function transaction(){
+        return $this->hasOne( Transaction::class, 'id', 'transactions_id' );
+    }
 }
