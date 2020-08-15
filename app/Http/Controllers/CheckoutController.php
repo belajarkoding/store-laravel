763a3516fc3192967252bc629846551467902776
Galih Pratama <hanamura.iost@gmail.com>
2020-08-15 15:27:16 +0700

    Menghapus Data Cart Setelah Checkout

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -47,6 +47,11 @@ class CheckoutController extends Controller
             ]);
         }
 
+        // Delete cart data
+        Cart::with(['product','user'])
+                ->where('users_id', Auth::user()->id)
+                ->delete();
+
         // Konfigurasi midtrans
         Config::$serverKey = config('services.midtrans.serverKey');
         Config::$isProduction = config('services.midtrans.isProduction');
