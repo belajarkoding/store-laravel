857f2c8e24744153a0cfe85ec64fa1cc0506278f
Galih Pratama <hanamura.iost@gmail.com>
2020-08-15 18:07:13 +0700

    Integrasi Controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\TransactionDetail;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('pages.dashboard');
+        $transactions = TransactionDetail::with(['transaction.user','product.galleries'])
+                            ->whereHas('product', function($product){
+                                $product->where('users_id', Auth::user()->id);
+                            });
+
+        $revenue = $transactions->get()->reduce(function ($carry, $item) {
+            return $carry + $item->price;
+        });
+
+        $customer = User::count();
+
+        return view('pages.dashboard',[
+            'transaction_count' => $transactions->count(),
+            'transaction_data' => $transactions->get(),
+            'revenue' => $revenue,
+            'customer' => $customer,
+        ]);
     }
 }
