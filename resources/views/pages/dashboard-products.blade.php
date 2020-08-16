741823ec6c1ed1246ef0253cfae7167c1208cb91
Galih Pratama <hanamura.iost@gmail.com>
2020-08-16 18:11:49 +0700

    Typo saeutik

--- resources/views/pages/dashboard-products.blade.php
+++ resources/views/pages/dashboard-products.blade.php
@@ -32,7 +32,7 @@
                   @foreach ($products as $product)
                       <div class="col-12 col-sm-6 col-md-4 col-lg-3">
                         <a
-                          href="{{ route('dashboard-product-details', $product->id) }}l"
+                          href="{{ route('dashboard-product-details', $product->id) }}"
                           class="card card-dashboard-product d-block"
                         >
                           <div class="card-body">
